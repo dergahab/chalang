9046efcf26801ef72a6e18858ea798fd89380496
dergah.ab <dergah.ab@gmail.com>
2025-06-13 01:35:48 +0400

    Update DatabaseSeeder to include LangSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            LanguageSeeder::class,
             UserSeeder::class,
             BannerSeeder::class,
             AboutSeeder::class,

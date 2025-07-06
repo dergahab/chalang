25f944e9ee1efb86d6445816ab55886315274252
dergahab <dergah.ab@gmail.com>
2025-07-07 01:02:35 +0400

    Add .gitignore to ignore contents of storage/framework except subfolders

--- resources/views/admin/pages/portfolio/in_main.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- Checked switch -->
-<div class="form-check form-switch">
-    <input class="form-check-input in_main" style="padding: 15px; width:60px" data-id="{{$item->id}}" type="checkbox" role="switch" id="in_main-{{$item->id}}" @if($item->in_main) checked @endif size="500" />
-    <label class="form-check-label" for="in_main-{{$item->id}}"></label>
-</div>
-  

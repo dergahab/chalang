ba078170daae9abb88ac85a41ee09f62c08fe75e
Dergah_Abdullayev <44070227+dergahab@users.noreply.github.com>
2025-07-17 01:38:10 +0400

    Update _form.blade.php

--- resources/views/admin/pages/service/_form.blade.php
+++ resources/views/admin/pages/service/_form.blade.php
@@ -58,7 +58,7 @@
             <input name="icon" class="form-control filestyle file" type="file" data-buttonname="btn-secondary"
                 @if (request()->routeIs('admin.service.create')) required @endif>
         </div>
-        <img src="{{ asset(Storage::url($item->icon)) }}"
+        <img src="{{ asset(Storage::url('/public/'.$item->icon)) }}"
             class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}"
             alt="">
     </div>
@@ -68,7 +68,7 @@
             <label for="file" class="form-label ">Şəkil <span class="text-danger">610x460</span></label>
             <input name="image" class="form-control filestyle file" type="file" data-buttonname="btn-secondary">
         </div>
-        <img src="{{ asset(Storage::url($item->image)) }}"
+        <img src="{{ asset(Storage::url('/public/'.$item->image)) }}"
             class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}"
             alt="">
     </div>
